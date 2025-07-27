28c191dfcfa5c7903275e52c3636ddd707ae6469
admin <crcool1106@gmail.com>
2025-07-27 11:12:47 +0530

    Remove all files from repository

--- restaurant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from django.urls import path 
-from .views import sayHello 
-from . import views
-
-urlpatterns = [ 
-    #path('', sayHello, name='sayHello'), 
-    path('', views.index, name='index')
-]
